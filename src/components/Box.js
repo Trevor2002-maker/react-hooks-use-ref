17d94a2b34f93785a54b255b2163f50c9b67bf9d
trevormadaramoringaschool <trevor.madara@student.moringaschool.gcom>
2023-04-28 09:37:59 +0300

    N

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,14 +1,1 @@
-import React, { useRef } from "react";
 
-function Box() {
-  const elementRef = useRef();
-
-  return (
-    <div ref={elementRef}>
-      <h1>Box</h1>
-      <button>Measure</button>
-    </div>
-  );
-}
-
-export default Box;
